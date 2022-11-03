783de09c4c16768ad5ec7f51957b384a5e7ab4a6
rkawk1234 <okus6977@gmail.com>
2022-11-03 04:09:33 -0400

    livescore page and pastgame page renders perfectly!

--- client/src/routes/Livescore.js
+++ client/src/routes/Livescore.js
@@ -1,5 +1,74 @@
+import {useEffect} from 'react'
+
 const Livescore = () => {
-    return <h2>Live score content</h2>;
+    useEffect(() => {
+        const fetch = require('node-fetch');
+    
+        const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all&timezone=America%2FToronto'
+      
+        const options = {
+          method: 'GET',
+          headers: {
+            'X-RapidAPI-Key': 'a7bc8e7b5fmsh878701ce1dc6cb3p1d0d8fjsn07f3e8faa102',
+            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+          }
+        };
+      
+        fetch(url, options)
+          .then(res =>
+            res.json()
+          )
+          .then(json => {
+    
+      
+            for (let i = 0; i < json.response.length; i++) {
+      
+              document.getElementById("liveData").innerHTML +=
+               "<tr> <td><img src=" + json.response[i].teams.home.logo + 
+               "><img/></td> <td>" + json.response[i].teams.home.name + 
+               "</td> <td>" + json.response[i].goals.home + "</td> <td>"+json.response[i].fixture.status.elapsed + "'</td><td>" 
+               + json.response[i].goals.away + " </td>  <td>" + json.response[i].teams.away.name 
+               + "</td>  <td> <img src=" + json.response[i].teams.away.logo + "><img/></td> </tr>"
+            }
+      //json.response[i].fixture.timestamp = start time 
+      
+          })
+          .catch(err => console.error('error:' + err));
+    
+      })
+    
+
+
+    return <div className='h-full w-full bg-stone-300'>
+
+    <table id="liveTable" className="h-screen w-full text-center  ">
+      <thead>
+        <tr >
+          <th className='w-1/12'></th>
+          <th>Home</th>
+
+          <th>Home Score</th>
+          <th className='w-1/12'></th>
+          <th>Away Score</th>
+
+          <th>Away</th>
+          <th className='w-1/12'></th>
+        </tr>
+      </thead>
+
+      <tbody id="liveData" className="justiy-evenly">
+
+
+
+      </tbody>
+
+
+    </table>
+
+
+
+  </div>
+
 }
 
 export default Livescore
--- client/src/routes/Pastgames.js
+++ client/src/routes/Pastgames.js
@@ -1,7 +1,87 @@
+import React, { useRef, useEffect, useState } from 'react'
 
 
 const Pastgames = () => {
-    return <h2>Past Game content</h2>;
+    const inputRef = useRef(null)
+    const [updated, setUpdated] = useState('')
+
+    const handleClick = () => {
+        document.getElementById("liveData").innerHTML = ""
+        setUpdated(inputRef.current.value)
+    }
+
+
+    useEffect(() => {
+        const fetch = require('node-fetch');
+
+        const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=' + updated + '&timezone=America%2FToronto'
+
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'a7bc8e7b5fmsh878701ce1dc6cb3p1d0d8fjsn07f3e8faa102',
+                'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+            }
+        };
+
+        fetch(url, options)
+            .then(res => res.json())
+            .then(json => {
+                for (let i = 0; i < json.response.length / 10; i++) {
+                    document.getElementById("liveData").innerHTML +=
+                        "<tr> <td><img src=" + json.response[i].teams.home.logo +
+                        "><img/></td> <td>" + json.response[i].teams.home.name +
+                        "</td> <td>" + json.response[i].goals.home + "</td> <td>" + json.response[i].fixture.date + "</td><td>"
+                        + json.response[i].goals.away + " </td>  <td>" + json.response[i].teams.away.name
+                        + "</td>  <td> <img src=" + json.response[i].teams.away.logo + "><img/></td> </tr>"
+
+                }
+
+            }
+            )
+            .catch(err => console.error('error:' + err));
+    })
+
+    return <div className='h-full w-full bg-stone-400'>
+
+        <div >
+            <input
+                className='border border-black'
+                placeholder='Enter Year'
+                ref={inputRef}
+                type="text"
+                name="year"></input>
+            <button className="bg-black text-white"
+                onClick={handleClick}>Search </button>
+        </div>
+
+
+        <table id="liveTable" className="w-full h-screen text-center">
+            <thead>
+                <tr >
+                    <th></th>
+                    <th>Home</th>
+
+                    <th>Home Score</th>
+                    <th>Away Score</th>
+
+                    <th>Away</th>
+                    <th></th>
+                </tr>
+            </thead>
+
+            <tbody id="liveData" className="text-black">
+
+
+            </tbody>
+
+
+        </table>
+
+
+
+    </div>
 };
 
 export default Pastgames
